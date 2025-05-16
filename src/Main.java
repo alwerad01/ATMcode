bab2f20ce71d6db7a345f476b64bc50bb10dc1ab
ALWERAD KHAN <alwerad01@gmail.com>
2025-05-16 19:56:05 +0500

    flow

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         Bank account1 = new Bank();
-//        account1.MAINMENU();
-        account1.CREATEACCOUNT();
+        account1.MAINMENU();
     }
 }
