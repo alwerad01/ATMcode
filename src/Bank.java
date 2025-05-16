28f1cf32284859c37955812e60a11a30880a01a9
ALWERAD KHAN <alwerad01@gmail.com>
2025-05-16 19:51:05 +0500

    Added main menu option and fixed account creation flow

--- src/Bank.java
+++ src/Bank.java
@@ -5,100 +5,136 @@ public class Bank {
 
     Scanner input = new Scanner(System.in);
 
-    public double Balance ;
-    public String Name ;
-    public int PIN ;
+    public double Balance;
+    public String Name = "user";
+    public int PIN = 1234 ;
 
-    public void MENU (){
-        System.out.println("Welcome to EasyBank! \n 1 : Log In \n 2 : Create Account ");
+    public void MAINMENU() {
+        System.out.print("Welcome to EasyBank! \n 1 : Log In \n 2 : Create Account\n 0 : Exit Program\n   : ");
         int MenuOption = input.nextInt();
 
-        if (MenuOption == 1 ){
-
+        if (MenuOption == 1) {
             LOGIN();
-
-        }
-
-        System.out.print("Enter Your Initial Deposit Amount : ");
-        int initial_deposit = input.nextInt();
-        Balance += initial_deposit ;
-
-        System.out.print(" 1 : Deposit\n 2 : Withdrawl\n 0 : Exit Program \n   : ");
-        int Menu_Option = input.nextInt();
-
-        if (Menu_Option == 1){
-            DEPOSIT();
-        }else if (Menu_Option == 2) {
-            WITHDRAWL();
-        }else {
+        } else if (MenuOption == 2) {
+            CREATEACCOUNT();
+        }else if (MenuOption == 0){
             System.out.println("Thank You For Using Our Program!");
         }
     }
 
-    public void CREATEACCOUNT (){
-        System.out.println("Enter Your Name : ");
-        String Name = input.nextLine();
-        System.out.println("Create A PIN ");
-        int PIN = input.nextInt();
+    public void CREATEACCOUNT() {
+        input.nextLine();
+        System.out.print("Enter Your Name : ");
+        Name = input.nextLine();
+        System.out.print("Create A PIN ");
+        PIN = input.nextInt();
+
         System.out.println(Name + " Your Account Has Been Created Sucessfully!");
 
+        System.out.print("Enter Your Initial Deposit Amount : ");
+        int initial_deposit = input.nextInt();
+        Balance += initial_deposit;
+        System.out.println(initial_deposit + "$ Has Been Deposited to Your Account!");
+        System.out.println("Your Current Balance  :  " + Balance + "$");
+
+            System.out.print(" 1 : Deposit\n 2 : Withdrawl\n 3 : Main Menu \n 0 : Exit Program ");
+            int Menu_Option = input.nextInt();
+            if (Menu_Option == 1) {
+                DEPOSIT();
+            } else if (Menu_Option == 2) {
+                WITHDRAWL();
+            } else if (Menu_Option == 3) {
+                MAINMENU();
+            }else {
+                System.out.println("Thank You For Using Our Program!");
+            }
     }
 
-    public void LOGIN (){
+    public void LOGIN() {
+        input.nextLine();
         System.out.print("Enter Your Name : ");
         String name = input.nextLine();
         System.out.print("Enter Your PIN : ");
         int Login_PIN = input.nextInt();
 
-        if (Objects.equals(name, Name) && Login_PIN == PIN ){
-            System.out.println("Welcome "+" To your Account ");
-        }
+        if (name.equalsIgnoreCase(Name) && Login_PIN == PIN) {
+            System.out.println("Welcome " + " To your Account ");
+            System.out.println("Name : " + Name);
+            System.out.println("Your Current Balance  :  " + Balance + "$");
 
-        System.out.println("YOU DONT HAVE ANY ACCOUNT");
-    }
-
-    public void DEPOSIT (){
-        System.out.print("Enter Your Deposit Amount X500: ");
-        int Deposit = input.nextInt();
-        if (Deposit % 500 == 0 && Deposit >= 500 ){
-            Balance += Deposit ;
-            System.out.println(Deposit + "$ Has Been Deposited to Your Account!");
-            System.out.println("Your Current Balance  :  " + Balance+ "$" );
-            System.out.print(" 1 : Deposit\n 2 : Withdrawl\n 0 : Exit Program \n   : ");
+            System.out.print(" 1 : Deposit\n 2 : Withdrawl\n 3 : Main Menu \n 0 : Exit Program\n  :  ");
             int Menu_Option = input.nextInt();
-            if (Menu_Option == 1){
+            if (Menu_Option == 1) {
                 DEPOSIT();
-            }else if (Menu_Option == 2) {
+            } else if (Menu_Option == 2) {
                 WITHDRAWL();
+            } else if (Menu_Option == 3) {
+                MAINMENU();
             }else {
                 System.out.println("Thank You For Using Our Program!");
             }
+
         }else {
-            System.out.println("Invalid Entry ");
+            System.out.println("User Not Found! ");
+
+            System.out.print(" 1 : For Try Again\n 2 : Create An Account\n 0 : Exit Program \n   : ");
+            int Menu_Option = input.nextInt();
+
+            if (Menu_Option == 1) {
+                LOGIN();
+            } else if (Menu_Option == 2) {
+                CREATEACCOUNT();
+            } else {
+                System.out.println("Thank You For Using Our Program!");
+            }
+        }
+    }
+
+    public void DEPOSIT() {
+        System.out.print("Enter Your Deposit Amount X500: ");
+        int Deposit = input.nextInt();
+        if (Deposit >= 0) {
+            Balance += Deposit;
+            System.out.println(Deposit + "$ Has Been Deposited to Your Account!");
+            System.out.println("Your Current Balance  :  " + Balance + "$");
+            System.out.print(" 1 : Deposit\n 2 : Withdrawl\n 3 : Main Menu\n 0 : Exit Program  : ");
+            int Menu_Option = input.nextInt();
+            if (Menu_Option == 1) {
+                DEPOSIT();
+            } else if (Menu_Option == 2) {
+                WITHDRAWL();
+            } else if (Menu_Option == 3) {
+                MAINMENU();
+            } else {
+                System.out.println("Thank You For Using Our Program!");
+            }
+        } else {
+            System.out.println("Invalid Entry\n Enter 0 For Back Menu ");
             DEPOSIT();
         }
     }
 
-    public void WITHDRAWL  (){
+    public void WITHDRAWL() {
         System.out.print("Enter Your Withdrawal Amount : ");
         int Withdrawl = input.nextInt();
-        if (Withdrawl < Balance ){
-            Balance -= Withdrawl ;
+        if (Withdrawl < Balance) {
+            Balance -= Withdrawl;
             System.out.println(Withdrawl + "$ Has Been Withdrawed From Your Account!");
-            System.out.println("Your Current Balance  :  " + Balance+ "$" );
-            System.out.print(" 1 : Deposit\n 2 : Withdrawl\n 0 : Exit Program \n   : ");
+            System.out.println("Your Current Balance  :  " + Balance + "$");
+            System.out.print(" 1 : Deposit\n 2 : Withdrawl\n 3 : Main Menu\n 0 : Exit Program \n   : ");
             int Menu_Option = input.nextInt();
-            if (Menu_Option == 1){
+            if (Menu_Option == 1) {
                 DEPOSIT();
-            }else if (Menu_Option == 2) {
+            } else if (Menu_Option == 2) {
                 WITHDRAWL();
-            }else {
+            } else if (Menu_Option == 3) {
+                MAINMENU();
+            } else {
                 System.out.println("Thank You For Using Our Program!");
             }
-        }else {
+        } else {
             System.out.println("Invalid Entry ");
             WITHDRAWL();
         }
     }
-
 }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         Bank account1 = new Bank();
-        account1.MENU();
+//        account1.MAINMENU();
+        account1.CREATEACCOUNT();
     }
 }
